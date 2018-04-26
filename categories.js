1bd60de539058eb61facac636d3204923c239492
Alistair Lobo <anonytr0janali9@gmail.com>
2018-04-26 13:49:10 +0530

    Telegram node bot installed

--- categories.js
+++ categories.js
@@ -2,4 +2,15 @@ const Bot = require('node-telegram-bot-api');
 const request = require('request');
 const url = 'https://developers.zomato.com/api/v2.1/categories';
 const trigger = '/categories';
-const token = '584328858:AAH_VxFfeXT8-D9FuSNiZ3MofnAMzSTqvTo';
+const token = require('./token');
+
+var cat = function () {
+// to be done
+}
+
+var commands = {
+    categories = cat
+}
+
+var msgText = message.text;
+
---
+++ token.js
@@ -0,0 +1,1 @@
+module.exports = '584328858:AAH_VxFfeXT8-D9FuSNiZ3MofnAMzSTqvTo';
