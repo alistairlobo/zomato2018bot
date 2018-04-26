6a771627b688ac5374e40e6ea4b190e3052639c9
Alistair Lobo <anonytr0janali9@gmail.com>
2018-04-26 12:47:47 +0530

    Access token added

---
+++ index.js
@@ -0,0 +1,5 @@
+const Bot = require('node-telegram-bot-api');
+const request = require('request');
+const url = 'https://developers.zomato.com/api/v2.1/categories';
+const trigger = '/categories';
+const token = '584328858:AAH_VxFfeXT8-D9FuSNiZ3MofnAMzSTqvTo';
